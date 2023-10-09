25c3c4183459ae73485d9ef28d787b5a35e3a290
Stanislav Vysotskyi <ardenhide@gmail.com>
2023-10-09 14:40:32 +0300

    add `KYC.DataBase` project

---
+++ src/KYC.DataBase/KycDbContext.cs
@@ -0,0 +1,17 @@
+﻿using KYC.DataBase.Models;
+using Microsoft.EntityFrameworkCore;
+using ConfiguredSqlConnection.Extensions;
+
+namespace KYC.DataBase;
+
+public class KycDbContext : DbContext
+{
+    public virtual DbSet<User> Users { get; set; } = null!;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder
+            .ConfigureFromActionConnection()
+            .ConfigureFromSecretConnection();
+    }
+}
