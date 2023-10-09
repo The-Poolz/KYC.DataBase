a4a3ee20f528ea7001e9a3144ed6b5bc98a51104
Stanislav Vysotskyi <ardenhide@gmail.com>
2023-10-09 18:49:15 +0300

    add init migration

---
+++ src/KYC.DataBase/Migrations/20231009154849_Init.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KYC.DataBase.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Guid = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    ClientId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RecordId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RefId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SubmitCount = table.Column<int>(type: "int", nullable: true),
+                    BlockPassID = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsArchived = table.Column<bool>(type: "bit", nullable: true),
+                    InreviewDate = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    WaitingDate = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ApprovedDate = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => new { x.Guid, x.Status });
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
