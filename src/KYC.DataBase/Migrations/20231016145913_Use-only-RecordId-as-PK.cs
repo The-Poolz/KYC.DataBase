d1ef1b450b04bbb905a05eefdc09568f06760493
Stanislav Vysotskyi <ardenhide@gmail.com>
2023-10-16 17:59:58 +0300

    add migration

---
+++ src/KYC.DataBase/Migrations/20231016145913_Use-only-RecordId-as-PK.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KYC.DataBase.Migrations
+{
+    /// <inheritdoc />
+    public partial class UseonlyRecordIdasPK : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Users",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "RefId",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Users",
+                table: "Users",
+                column: "RecordId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Users",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "RefId",
+                table: "Users",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Users",
+                table: "Users",
+                columns: new[] { "RecordId", "RefId" });
+        }
+    }
+}
